46845304fa502d5206ea7b12ec15a843f50a1a13
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2023-03-03 23:22:13 -0500

    Added queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,42 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USERS = gql`
+  query allUsers {
+    users {
+      _id
+      username
+      email
+      savedBooks
+    }
+  }
+`;
+
+export const QUERY_SINGLE_USER = gql`
+  query singleUser($userId: ID!) {
+    user(userId: $userId) {
+      _id
+      username
+      email
+      savedBooks
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
