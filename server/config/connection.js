42c67af62c8d48a081943133b97671d989f05250
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2023-03-05 17:56:10 -0500

    Final edits

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Module-21-Challenge', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
